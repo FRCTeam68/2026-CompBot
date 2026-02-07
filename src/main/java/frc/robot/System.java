03799e4c34afa8de4ab6296428c66846d9fbaa2a
ToryFisher <torynfisher@gmail.com>
2026-02-07 09:49:40 -0500

    Merged from main

---
+++ src/main/java/frc/robot/System.java
@@ -0,0 +1,101 @@
+package frc.robot;
+
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.DriveConstants;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOReal;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.intake.IntakePivotIO;
+import frc.robot.subsystems.rollers.RollerSystem;
+import frc.robot.subsystems.rollers.RollerSystemIO;
+import frc.robot.subsystems.shooter.Shooter;
+import frc.robot.subsystems.vision.Vision;
+import frc.robot.subsystems.vision.VisionConstants.CameraInfo;
+import frc.robot.subsystems.vision.VisionIO;
+import frc.robot.subsystems.vision.VisionIOLimelight;
+import lombok.Getter;
+
+public class System {
+  private static System instance = null;
+
+  // Subsystems
+  @Getter private final Drive drive;
+  @Getter private final Vision vision;
+  @Getter private final Intake intakePivot;
+  @Getter private final RollerSystem intakeSpin;
+  @Getter private final Shooter shooter;
+  @Getter private final RollerSystem spindexer;
+  @Getter private final RollerSystem feeder;
+
+  public System() {
+    switch (Constants.getMode()) {
+      case REAL:
+        drive =
+            new Drive(
+                new GyroIOPigeon2(),
+                new ModuleIOReal(DriveConstants.moduleConfigs[0]),
+                new ModuleIOReal(DriveConstants.moduleConfigs[1]),
+                new ModuleIOReal(DriveConstants.moduleConfigs[2]),
+                new ModuleIOReal(DriveConstants.moduleConfigs[3]));
+
+        vision =
+            new Vision(
+                drive::addVisionMeasurement,
+                drive::getPose,
+                drive::getFieldVelocity,
+                new VisionIOLimelight(CameraInfo.LL_4),
+                new VisionIOLimelight(CameraInfo.LL_3G));
+        break;
+
+      case SIM:
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim());
+
+        vision = new Vision(drive::addVisionMeasurement, drive::getPose, drive::getFieldVelocity);
+        break;
+
+      default:
+        drive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+
+        vision =
+            new Vision(
+                drive::addVisionMeasurement,
+                drive::getPose,
+                drive::getFieldVelocity,
+                new VisionIO() {},
+                new VisionIO() {});
+    }
+
+    intakePivot = new Intake(new IntakePivotIO() {});
+    intakeSpin = new RollerSystem("null1", new RollerSystemIO() {});
+    shooter = new Shooter();
+    spindexer = new RollerSystem("null2", new RollerSystemIO() {});
+    feeder = new RollerSystem("null3", new RollerSystemIO() {});
+  }
+
+  /**
+   * Gets the single instance of the System class.
+   *
+   * @return The single instance of the System class.
+   */
+  public static System getInstance() {
+    if (instance == null) {
+      instance = new System();
+    }
+    return instance;
+  }
+}
---
+++ src/main/java/frc/robot/commands/auton/AutonSequenceRightTrench.java
@@ -0,0 +1,72 @@
+package frc.robot.commands.auton;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.DeferredCommand;
+import frc.robot.System;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.util.AutonUtil;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class AutonSequenceRightTrench implements AutonSequence {
+  // Complete system
+  private static final System system = System.getInstance();
+
+  // Subsystems
+  private static final Drive drive = system.getDrive();
+
+  private static final List<String> pathNames = new ArrayList<>();
+  private boolean climbpath = false;
+
+  public AutonSequenceRightTrench(boolean climb) {
+    for (Path path : Path.values()) {
+      pathNames.add(path.getPathName());
+    }
+    this.climbpath = climb;
+  }
+
+  private enum Path {
+    RA("Right Trench A"),
+    RB("Right Trench B"),
+    RC("Right Trench C"),
+    RD("Right Trench C 2");
+
+    private String pathName;
+
+    private Path(String pathName) {
+      this.pathName = pathName;
+    }
+
+    private String getPathName() {
+      return pathName;
+    }
+  }
+
+  @Override
+  public List<String> getPathNames() {
+    return pathNames;
+  }
+
+  @Override
+  public Command sequence() {
+    return new DeferredCommand(
+        () -> {
+          // initialization
+          Command command =
+              Commands.sequence(
+                  // Shoot preload note
+                  AutonUtil.followPath(Path.RA.getPathName()),
+                  AutonUtil.followPath(Path.RB.getPathName()));
+
+          if (this.climbpath) {
+            command = command.andThen(AutonUtil.followPath(Path.RC.getPathName()));
+          } else {
+            command = command.andThen(AutonUtil.followPath(Path.RD.getPathName()));
+          }
+          return command;
+        },
+        Set.of(drive));
+  }
+}
