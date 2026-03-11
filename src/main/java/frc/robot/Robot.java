54e7c1a0c0bf7586492476e4a8519d5b57c4b1f2
spaceZee <pittiglio01@gmail.com>
2026-03-11 01:59:41 -0400

    - Added back null check for auton command before scheduling

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -241,7 +241,10 @@ public class Robot extends LoggedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
-    CommandScheduler.getInstance().schedule(Auton.SelectedCommand());
+    autonomousCommand = Auton.SelectedCommand();
+    if (autonomousCommand != null) {
+      CommandScheduler.getInstance().schedule(autonomousCommand);
+    }
 
     if (DriverStation.isFMSAttached()) {
       if (AllianceFlipUtil.shouldFlip()) {
