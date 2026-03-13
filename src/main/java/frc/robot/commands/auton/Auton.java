e7e8508fed0f784a93b6db6fd691c576e9245369
jashow55 <jason.howerton@gmail.com>
2026-03-13 18:25:44 -0400

    -update Left_LigerBot auton, Center, LeftDefault and RightDefault. -tested on simulator.
    -probably will replace defaults in future with Liger and 1768 as we are not likely to use these current defaults.

--- src/main/java/frc/robot/commands/auton/Auton.java
+++ src/main/java/frc/robot/commands/auton/Auton.java
@@ -126,11 +126,11 @@ public class Auton {
             return Left_LigerBot();
           case None:
           default:
-            return Apollo();
+            return LeftDefault();
         }
 
       case Center:
-        return Terra();
+        return CenterDefault();
 
       case Right:
         switch (autonSpecial.get()) {
@@ -142,7 +142,7 @@ public class Auton {
             return Right_1768();
           case None:
           default:
-            return Neptune();
+            return RightDefault();
         }
 
       default:
@@ -150,89 +150,100 @@ public class Auton {
     }
   }
 
-  private static Command Terra() {
+  // -----------------------------------------------------------------------------------------
+  private static Command delayShooterStart(double turretPosition) {
+    return Commands.sequence(
+            // Make sure flywheels don't start
+            ShooterCommands.runStatic(
+                0, shooter.getHood().getElevation(), shooter.getTurret().getPosition()),
+            Commands.waitSeconds(1),
+            // Move to the setpoints that will be called when the robot enters the
+            // alliance zone again
+            // ShooterCommands.runStatic(57, 63, 275),
+            ShooterCommands.runStatic(57, 63, turretPosition),
+            // enable automatic control
+            // The shooter will remain at the previous setpoint until it enters
+            // the alliance zone
+            ShooterCommands.clearStaticSetpoint())
+        .withName("DelayShooterStart");
+  }
+
+  private static Command WaitWhileShootThenAgitate() {
+    return Commands.sequence(Commands.waitSeconds(3), IntakeCommands.agitate().withTimeout(1))
+        .withName("WaitWhileShootThenAgitate");
+  }
+
+  private static Command CenterDefault() {
     return new DeferredCommand(
             () -> {
               Command myCommand1;
               Command myCommand2 = Commands.none();
+
               if (autonDepot.get()) {
 
                 myCommand1 =
                     Commands.sequence(
-                        PathUtil.followPath("Center Depot"),
-                        // Commands.parallel(
-                        //     PathUtil.followPath("Center Depot"),
-                        //     Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
-                        // IntakeCommands.stop(),
-                        ShooterCommands.shoot(false).repeatedly().withTimeout(5.0),
+                        Commands.parallel(
+                            PathUtil.followPath("Center Depot"),
+                            Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
+                        Commands.waitSeconds(3).andThen(IntakeCommands.stopSpin()),
                         PathUtil.followPath("Depot Tower"));
 
                 if (autonOutpost.get()) {
-                  myCommand2 = PathUtil.followPath("Tower Outpost");
-                  myCommand2 =
-                      myCommand2.andThen(
-                          ShooterCommands.shoot(false).repeatedly().withTimeout(5.0));
+                  myCommand2 = Commands.sequence(PathUtil.followPath("Tower Outpost"));
                 }
               } else {
-                myCommand1 =
-                    Commands.sequence(
-                        PathUtil.followPath("Center Outpost"),
-                        ShooterCommands.shoot(false).repeatedly().withTimeout(5.0));
+                myCommand1 = Commands.sequence(PathUtil.followPath("Center Outpost"));
               }
 
               return myCommand1.andThen(myCommand2);
             },
             Set.of(drive))
-        .withName("Auton_Terra");
+        .withName("Auton_CenterDefault");
   }
 
-  private static Command delayShooterStart() {
-    return Commands.sequence(
-        // Make sure flywheels don't start
-        ShooterCommands.runStatic(
-            0, shooter.getHood().getElevation(), shooter.getTurret().getPosition()),
-        Commands.waitSeconds(1),
-        // Move to the setpoints that will be called when the robot enters the
-        // alliance zone again
-        ShooterCommands.runStatic(57, 63, 275),
-        // enable automatic control
-        // The shooter will remain at the previous setpoint until it enters
-        // the alliance zone
-        ShooterCommands.clearStaticSetpoint());
-  }
-
-  private static Command Apollo() {
+  private static Command LeftDefault() {
     return new DeferredCommand(
             () -> {
               Command myCommand1;
               Command myCommand2;
+              Command neutralPath1Command;
+              Command toDepotCommand;
 
-              myCommand1 =
+              neutralPath1Command =
                   Commands.sequence(
                       Commands.parallel(
                           PathUtil.followPath("Left Trench A"),
-                          delayShooterStart(),
+                          delayShooterStart(90),
                           Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
-                      IntakeCommands.stopSpin(),
                       PathUtil.followPath("Left Trench B"),
-                      Commands.waitSeconds(
-                          3.0) // replace with ShootCommands.Shootloop.withTimeOut(3.0.)
-                      );
+                      IntakeCommands.stopSpin());
 
-              if (autonDepot.get()) {
-                myCommand2 = PathUtil.followPath("Left Depot");
-                myCommand2 =
-                    myCommand2.andThen(
-                        Commands.waitSeconds(
-                            3.0)); // replace with ShootCommands.Shootloop.withTimeOut(3.0.)
+              toDepotCommand =
+                  Commands.sequence(
+                      Commands.parallel(
+                          PathUtil.followPath("Left Depot1"),
+                          ShooterCommands.dontShoot().withTimeout(2),
+                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
+                      PathUtil.followPath("Left Depot2"));
+
+              if (!autonDepot.get()) {
+                // run single pass to nuetral zone
+                myCommand1 = Commands.sequence(neutralPath1Command, WaitWhileShootThenAgitate());
+                myCommand2 = Commands.none();
+
               } else {
-                myCommand2 = PathUtil.followPath("Left Free Seconds");
+                myCommand1 = neutralPath1Command;
+                myCommand2 =
+                    Commands.sequence(
+                        toDepotCommand, // drive to depot while shooting
+                        WaitWhileShootThenAgitate());
               }
 
               return myCommand1.andThen(myCommand2);
             },
             Set.of(drive))
-        .withName("Auton_Apollo");
+        .withName("Auton_LeftDefault");
   }
 
   private static Command Left_LigerBot() {
@@ -240,38 +251,51 @@ public class Auton {
             () -> {
               Command myCommand1;
               Command myCommand2;
+              Command neutralPath1Command;
+              Command neutralPath2Command;
+              Command toDepotCommand;
 
-              myCommand1 =
+              neutralPath1Command =
                   Commands.sequence(
                       Commands.parallel(
                           PathUtil.followPath("Left Trench A1"),
+                          delayShooterStart(90),
                           Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
                       PathUtil.followPath("Left Trench A2"),
-                      IntakeCommands.stopSpin(),
                       PathUtil.followPath("Left Trench B1"),
-                      ShooterCommands.shoot(false).withTimeout(3.0));
+                      IntakeCommands.stopSpin());
 
-              if (autonDepot.get()) {
-                myCommand2 =
+              neutralPath2Command =
+                  Commands.sequence(
+                      Commands.parallel(
+                          PathUtil.followPath("Left Trench C1"),
+                          delayShooterStart(90),
+                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
+                      PathUtil.followPath("Left Trench D1"),
+                      IntakeCommands.stopSpin());
+
+              toDepotCommand =
+                  Commands.sequence(
+                      Commands.parallel(
+                          PathUtil.followPath("Left Depot1"),
+                          ShooterCommands.dontShoot().withTimeout(2),
+                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
+                      PathUtil.followPath("Left Depot2"));
+
+              if (!autonDepot.get()) {
+                // run double pass to nuetral zone
+                myCommand1 =
                     Commands.sequence(
-                        PathUtil.followPath("Left Depot1"),
-                        Commands.parallel(
-                            PathUtil.followPath("Left Depot2"),
-                            IntakeCommands.intakeRemainOn(),
-                            ShooterCommands.shoot(false).withTimeout(5.0)),
-                        IntakeCommands.stopSpin());
+                        neutralPath1Command, WaitWhileShootThenAgitate(), neutralPath2Command);
+                myCommand2 = Commands.none();
+
               } else {
+                myCommand1 = neutralPath1Command;
                 myCommand2 =
                     Commands.sequence(
-                        Commands.parallel(
-                            PathUtil.followPath("Left Trench C1"),
-                            Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
-                        IntakeCommands.stopSpin(),
-                        PathUtil.followPath("Left Trench D1"),
-                        ShooterCommands.shoot(false).withTimeout(5.0),
-                        IntakeCommands.stopSpin());
+                        toDepotCommand, // drive to depot while shooting
+                        WaitWhileShootThenAgitate());
               }
-
               return myCommand1.andThen(myCommand2);
             },
             Set.of(drive))
@@ -283,24 +307,22 @@ public class Auton {
             () -> {
               Command myCommand1;
               Command myCommand2;
-              Command neutralPass1Command;
-              Command neutralPass2Command;
+              Command neutralPath1Command;
+              Command neutralPath2Command;
               Command toOutpostCommand;
-              Command fromOutpostCommand;
 
-              neutralPass1Command =
+              neutralPath1Command =
                   Commands.sequence(
                       Commands.parallel(
                           PathUtil.followPath("Right_1768_AB1"),
-                          delayShooterStart(),
-                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
-                      IntakeCommands.stopSpin());
+                          delayShooterStart(275),
+                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())));
 
-              neutralPass2Command =
+              neutralPath2Command =
                   Commands.sequence(
                       Commands.parallel(
                           PathUtil.followPath("Right_1768_CD1"),
-                          delayShooterStart(),
+                          delayShooterStart(275),
                           Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
                       IntakeCommands.stopSpin());
 
@@ -309,27 +331,27 @@ public class Auton {
                       Commands.parallel(
                           PathUtil.followPath("Right_1768_Outpost_1"),
                           Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())));
-
+              // -----------------------------------------------------
               if (!autonOutpost.get()) {
+                // run double pass to nuetral zone
                 myCommand1 =
                     Commands.sequence(
-                        neutralPass1Command,
-                        Commands.waitSeconds(1)
-                            .andThen(IntakeCommands.agitate(1.0).repeatedly())
-                            .withTimeout(3),
-                        neutralPass2Command);
-                myCommand2 = toOutpostCommand;
+                        neutralPath1Command,
+                        WaitWhileShootThenAgitate(),
+                        neutralPath2Command,
+                        WaitWhileShootThenAgitate());
+                myCommand2 = Commands.none();
               } else {
-                myCommand1 = neutralPass1Command;
+                // run single pass to nuetral zone, then to outpost while shooting, wait at outpost
+                // to shoot, then drive back to trench while shooting
+                myCommand1 = neutralPath1Command;
                 myCommand2 =
                     Commands.sequence(
-                        Commands.waitSeconds(5),
-                        // .andThen(IntakeCommands.agitate(1.0).repeatedly())
-                        // .withTimeout(5), // wait for fuel to dump into hopper
-                        Commands.deadline(
-                                PathUtil.followPath("Right_1768_Outpost_2"),
-                                ShooterCommands.shoot(false).repeatedly())
-                            .withTimeout(5.0));
+                        toOutpostCommand,
+                        Commands.waitSeconds(2), // for hopper dump of fuels to finish
+                        Commands.parallel(
+                            PathUtil.followPath("Right_1768_Outpost_2"),
+                            WaitWhileShootThenAgitate()));
               }
 
               return myCommand1.andThen(myCommand2);
@@ -338,37 +360,48 @@ public class Auton {
         .withName("Auton_Right_1768");
   }
 
-  private static Command Neptune() {
+  private static Command RightDefault() {
     return new DeferredCommand(
             () -> {
               Command myCommand1;
               Command myCommand2;
+              Command neutralPath1Command;
+              Command toOutpostCommand;
 
-              myCommand1 =
+              neutralPath1Command =
                   Commands.sequence(
                       Commands.parallel(
                           PathUtil.followPath("Right Trench A"),
+                          delayShooterStart(90),
                           Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
-                      IntakeCommands.stopSpin(),
                       PathUtil.followPath("Right Trench B"),
-                      Commands.waitSeconds(
-                          3.0) // replace with ShootCommands.Shootloop.withTimeOut(3.0.)
-                      );
+                      IntakeCommands.stopSpin());
 
-              if (autonOutpost.get()) {
-                myCommand2 = PathUtil.followPath("Right Outpost");
-                myCommand2 =
-                    myCommand2.andThen(
-                        Commands.waitSeconds(
-                            3.0)); // replace with ShootCommands.Shootloop.withTimeOut(3.0.)
+              toOutpostCommand =
+                  Commands.sequence(
+                      Commands.parallel(
+                          PathUtil.followPath("Right Outpost"),
+                          ShooterCommands.dontShoot().withTimeout(2),
+                          Commands.waitSeconds(0.5).andThen(IntakeCommands.intakeRemainOn())),
+                      PathUtil.followPath("Outpost Tower"));
+
+              if (!autonOutpost.get()) {
+                // run single pass to nuetral zone
+                myCommand1 = Commands.sequence(neutralPath1Command, WaitWhileShootThenAgitate());
+                myCommand2 = Commands.none();
+
               } else {
-                myCommand2 = PathUtil.followPath("Right Free Seconds");
+                myCommand1 = neutralPath1Command;
+                myCommand2 =
+                    Commands.sequence(
+                        toOutpostCommand, // drive to outpost while shooting
+                        WaitWhileShootThenAgitate());
               }
 
               return myCommand1.andThen(myCommand2);
             },
             Set.of(drive))
-        .withName("Auton_Neptune");
+        .withName("Auton_RightDefault");
   }
 
   @SuppressWarnings("unused")
